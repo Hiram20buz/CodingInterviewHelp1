b4443567c27aa9d522eff54ae62f31fd56599305
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-02-13 11:47:57 -0800

    Update NumOfVowAndConsoInString.py

--- NumOfVowAndConsoInString.py
+++ NumOfVowAndConsoInString.py
@@ -1,1 +1,20 @@
 #How to calculate the number of vowels and consonants in a string?
+
+letras={}
+
+#string to list
+hello="hola hola". lower()
+list1=list(hello.replace(" ", ""))
+print(list1)
+letras["Vocales"]=0
+letras ["Consonantes"]=0
+print(letras)
+
+for letra in list1:
+    if(letra=="a" or letra=="e" or letra=="i" or letra=="o" or letra=="u"):
+        letras["Vocales"]+=1
+        
+    else:
+        letras["Consonantes"]+=1
+        
+print(letras)
